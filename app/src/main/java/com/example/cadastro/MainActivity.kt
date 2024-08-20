bd2b691ee2f46d529b3d6750b9fa96e362117bcd
Eduardo Derisso <dudu.derisso@gmail.com>
2024-08-19 22:02:37 -0300

    feat[MainActivity]: added logic of cleaning all input fiels, checkbox ou radio boxes

--- app/src/main/java/com/example/cadastro/MainActivity.kt
+++ app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -3,13 +3,31 @@ package com.example.cadastro
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.AdapterView.OnItemSelectedListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cadastro.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
+
+        amb.limparBt.setOnClickListener {
+            with (amb) {
+                nomeCompletoEt.setText("")
+                telefoneEt.setText("")
+                emailEt.setText("")
+                listaEmailCb.isChecked = false
+                sexoRg.clearCheck()
+                cidadeEt.setText("")
+                ufSp.setSelection(-1);
+            }
+        }
     }
 }
